4f835249708b3e78f01c0ab0f13aa6c5101c03ea
doreshnikov <dan.io.oreshnikov@gmail.com>
2023-09-20 05:28:10 +0300

    Connector to Courses [Implemented connector to Courses and auto-load from config]

--- algobot/data/connectors/courses.py
+++ algobot/data/connectors/courses.py
@@ -1,14 +1,20 @@
-from algobot.drivers.sqlite.models import Student, Transfer
+from algobot.drivers.sqlite import database
+from algobot.drivers.sqlite.models import Course
+from algobot.config import sheets_config
 
 
-class Transfers:
+class Courses:
     @staticmethod
-    def list_external_course_students(
-        course: str, group_id: str
-    ) -> list[tuple[str, str]]:
-        students = (
-            Transfer.select()
-            .join(Student)
-            .where((Transfer.course == course) & (Transfer.transfer_group == group_id))
-        )
-        return students
+    def list_courses() -> list[dict]:
+        return Course.select().dicts()
+
+    @staticmethod
+    def load_from_config(clear: bool = False):
+        with database.atomic():
+            if clear:
+                Course.delete().execute()
+            for course_config in sheets_config['courses']:
+                course = course_config['course']
+                for group in course_config['groups']:
+                    Course.get_or_create(course=course, group_id=group)
+
